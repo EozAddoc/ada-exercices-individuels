2a9fb84d71985baa86027bd1139694ff7bce08a6
EozAddoc <95304408+EozAddoc@users.noreply.github.com>
2022-06-10 14:46:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-function askName (
-let person = prompt ("Name?")
-var HiName = prompt ("bonjour" + " " + person)
-<h2> </h2> 
-document.body.innerHTML += '<h1> Coucou! </h1>’)
 
-function askBirthYear (
-let birthYear = prompt ("Marquer votre année de naissance")
-var a = birthYear
-document.body.innerHTML += '<h3> Vous avez + "Age" </h3>’
-var b = 2022
-alert = (b-a)
-prompt ("Votre âge est" + " " + alert )
-)
