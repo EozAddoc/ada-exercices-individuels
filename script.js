5c95b272cc836ccc3d3c6d228eed64945b63c4bb
EozAddoc <95304408+EozAddoc@users.noreply.github.com>
2022-06-10 12:30:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ let person = prompt ("Name?")
 var HiName = prompt ("bonjour" + " " + person)
 <h2> </h2> 
 document.body.innerHTML += '<h1> Coucou! </h1>’
+let birthYear = prompt ("Marquer votre année de naissance")
+var Age = (2022 - birthYear)
+document.body.innerHTML += '<h3> Vous avez + "Age" </h3>’
